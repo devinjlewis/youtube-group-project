de170ef3b3823afe24ab0fe3130651005962147c
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-05-29 10:06:24 -0400

    New Changes about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import jibonPic from "../assets/Jivon Paul 2 (1).jpg";
+import devinPic from "../assets/Jivon Paul 2 (1).jpg";
+import jorgePic from "../assets/Jivon Paul 2 (1).jpg";
+import "./About.css";
+import teamIcon from "../assets/Pursuit+Wordmark+White.png";
+
+function PersonCard({ name, image, email, skillset, bio }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div
+      className={`person-card ${isFlipped ? "flipped" : ""}`}
+      onClick={handleClick}
+    >
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img className="person-image" src={image} alt={name} />
+          <h2>{name}</h2>
+          <p className="person-title">Pursuit Fellow 9.5</p>
+        </div>
+        <div className="flip-card-back">
+          <div className="card-content">
+            <h3>{name}</h3>
+            <p>
+              <strong>Email:</strong> {email}
+            </p>
+            <p className="person-skillset">
+              <strong>Skillset:</strong> {skillset}
+            </p>
+          </div>
+          <p className="person-bio">{bio}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function About() {
   return (
     <div className="about-page">
-      <h1>About Page</h1>
-      <h2>Jibon</h2>
-      <h2>Devin</h2>
-      <h2>Jorge</h2>
+      <h1>Creators</h1>
+      <div className="person-cards">
+        <PersonCard
+          name="Jibon"
+          image={jibonPic}
+          email="jibon@gmail.com"
+          skillset={"JS, CSS, JavaScript, React, and others"}
+          bio="Jibon is a passionate developer with expertise in frontend technologies."
+        />
+        <PersonCard
+          name="Devin"
+          image={devinPic}
+          email="devin@gmail.com"
+          skillset={"JS, CSS, JavaScript, React, and others"}
+          bio="Devin is a skilled designer with a keen eye for aesthetics."
+        />
+        <PersonCard
+          name="Jorge"
+          image={jorgePic}
+          email="jorge@gmail.com"
+          skillset={"JS, CSS, JavaScript, React, and others"}
+          bio="Jorge is a talented full-stack developer with experience in building scalable web applications."
+        />
+      </div>
+      <div className="project-summary">
+        <h2>Project Summary</h2>
+        <p>
+          This project is a showcase of our team's skills and expertise in web
+          development. We have combined our frontend and design knowledge to
+          create an interactive and visually appealing website. Feel free to
+          explore and learn more about our team members and their contributions
+          to this project.
+        </p>
+        <img src={teamIcon} alt="Team Icon" className="team-icon" />
+      </div>
     </div>
   );
 }
