67fcf01145431f57f661f224f23b9ecb39d3c20f
Devin Lewis <devinlewis@pursuit.org>
2023-05-29 10:41:04 -0400

    Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,81 +6,83 @@ import SearchBox from "./SearchBox";
 import { Link } from "react-router-dom";
 
 function Home() {
-  const [searchInput, setSearchInput] = useState("");
-  const [isEmptySearch, setIsEmptySearch] = useState(true);
-  const [searchResults, setSearchResults] = useState([]);
-  const [selectedVideoId, setSelectedVideoId] = useState(null);
+    const [searchInput, setSearchInput] = useState("");
+    const [isEmptySearch, setIsEmptySearch] = useState(true);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedVideoId, setSelectedVideoId] = useState(null);
 
-  useEffect(() => {
-    setIsEmptySearch(searchInput.trim() === "");
-  }, [searchInput]);
+    useEffect(() => {
+        setIsEmptySearch(searchInput.trim() === "");
+    }, [searchInput]);
 
-  const handleSearch = () => {
-    if (searchInput.trim() === "") {
-      setIsEmptySearch(true);
-      setSearchResults([]);
-    } else {
-      setIsEmptySearch(false);
-      fetchVideos(searchInput);
-    }
-  };
+    const handleSearch = () => {
+        if (searchInput.trim() === "") {
+            setIsEmptySearch(true);
+            setSearchResults([]);
+        } else {
+            setIsEmptySearch(false);
+            fetchVideos(searchInput);
+        }
+    };
 
-  const fetchVideos = async (searchQuery) => {
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchQuery}&key=${apiKey}`;
+    const fetchVideos = async (searchQuery) => {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchQuery}&key=${apiKey}`;
 
-    try {
-      const response = await axios.get(url);
-      const data = response.data;
-      setSearchResults(data.items || []);
-    } catch (error) {
-      console.error("Error fetching videos:", error);
-      setSearchResults([]);
-    }
-  };
+        try {
+            const response = await axios.get(url);
+            const data = response.data;
+            setSearchResults(data.items || []);
+        } catch (error) {
+            console.error("Error fetching videos:", error);
+            setSearchResults([]);
+        }
+    };
 
-  const handleVideoClick = (videoId) => {
-    setSelectedVideoId(videoId);
-  };
+    const handleVideoClick = (videoId) => {
+        setSelectedVideoId(videoId);
+    };
 
-  return (
-    <div className="search-container">
-      <SearchBox
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        onSearch={handleSearch}
-      />
+    return (
+        <div className="search-container">
+            <SearchBox
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                onSearch={handleSearch}
+            />
 
-      {isEmptySearch && (
-        <p className="search-message">
-          No search yet! Please submit a search above.
-        </p>
-      )}
+            {isEmptySearch && (
+                <p className="search-message">
+                    No search yet! Please submit a search above.
+                </p>
+            )}
 
-      {selectedVideoId && (
-        <div className="video-player">
-          <YouTube videoId={selectedVideoId} />
+            {selectedVideoId && (
+                <div className="video-player">
+                    <YouTube videoId={selectedVideoId} />
+                </div>
+            )}
+
+            {searchResults && searchResults.length > 0 && (
+                <div className="search-results">
+                    {searchResults.map((video) => (
+                        <Link
+                            key={video.id.videoId}
+                            to={`/video/${video.id.videoId}`}
+                            className="video-link"
+                        >
+                            <VideoItem
+                                video={video}
+                                onClick={() =>
+                                    handleVideoClick(video.id.videoId)
+                                }
+                            />
+                        </Link>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-
-      {searchResults && searchResults.length > 0 && (
-        <div className="search-results">
-          {searchResults.map((video) => (
-            <Link
-              key={video.id.videoId}
-              to={`/video/${video.id.videoId}`}
-              className="video-link"
-            >
-              <VideoItem
-                video={video}
-                onClick={() => handleVideoClick(video.id.videoId)}
-              />
-            </Link>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Home;
