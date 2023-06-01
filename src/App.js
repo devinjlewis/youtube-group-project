d2ce92103449ae653f7acd152e750aa49edcd3c8
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-06-01 17:25:27 -0400

    now youtube clone works as a link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import VideoPage from "./components/VideoPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 
 function App() {
@@ -12,14 +17,22 @@ function App() {
     <Router>
       <div className="app">
         <header className="header">
-          <h1 className="title">YouTube Clone</h1>
+          <h1 className="title">
+            <Link to="/" className="title-link">
+              YouTube Clone
+            </Link>
+          </h1>
           <nav className="nav">
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
               </li>
               <li>
-                <Link to="/about">About</Link>
+                <Link to="/about" className="nav-link">
+                  About
+                </Link>
               </li>
             </ul>
           </nav>
@@ -29,6 +42,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/video/:videoId" element={<VideoPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
       </div>
